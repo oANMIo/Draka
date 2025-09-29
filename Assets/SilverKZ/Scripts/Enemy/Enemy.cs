4bc20f62df51e2a2a5192b884598738870ad97a1
SilverKZ <75739844+SilverKAZA@users.noreply.github.com>
2025-09-30 03:50:51 +0500

    Update Player, Enemy, SpawnEnemy

--- Assets/SilverKZ/Scripts/Enemy/Enemy.cs
+++ Assets/SilverKZ/Scripts/Enemy/Enemy.cs
@@ -1,22 +1,65 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+public enum EnemyType
+{
+    Idle,
+    Chase
+}
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _health = 100;
+    [SerializeField] private EnemyType _type;
 
-    private Collider2D _collider;
+    [Header("AI Movement")]
+    [SerializeField] private float _attackRange = 2.0f;
+    [SerializeField] private float _speed = 1.6f;
+    [SerializeField] private float _separationRadius = 2.5f;
+    [SerializeField] private float _maxForce = 5f;
+    [SerializeField] private float _slowingRadius = 2f;
+    [SerializeField] private float _attackCooldown = 1.5f;
+
+    private Vector2 _velocity;
+    private BoxCollider2D _collider;
+    private Rigidbody2D _rb;
     private Animator _animator;
     private bool _isDamage = false;
+    private bool _isAlive = true;
+    private float _lastAttackTime;
+
+    public static Action<int> onDeathEnemy;
+
+    public bool IsChase { get;  set; }
+    public Player Target { get;  set; }
+    public List<Enemy> Friends { private get; set; }
 
     private void Start()
     {
-        _collider = GetComponent<Collider2D>();
+        _collider = GetComponent<BoxCollider2D>();
         _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        AnimatorStateInfo state = _animator.GetCurrentAnimatorStateInfo(0);
+        _animator.Play(state.fullPathHash, -1, UnityEngine.Random.Range(0f, 1f));
+        _animator.speed = UnityEngine.Random.Range(0.9f, 1.1f);
+        Friends = new List<Enemy>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        if (_isAlive == false) return;
+  
+        Flip();
+        Move();
+
         _animator.SetBool("Damage", _isDamage);
+        _animator.SetFloat("Speed", _velocity.sqrMagnitude);
+    }
+
+    public void DeathEnemy()
+    {
+        onDeathEnemy?.Invoke(1);
     }
 
     public void SetTriggerDamage()
@@ -42,7 +85,103 @@ public class Enemy : MonoBehaviour
     private void Die()
     {
         AudioManager.Instance.Play(AudioManager.Clip.MaleDamage);
+        _isAlive = false;
         _collider.enabled = false;
         _animator.SetBool("Die", true);
+        DeathEnemy();
+    }
+
+    private void Flip()
+    {
+        if (Target == null) return;
+        
+        transform.localScale = new Vector3(Mathf.Sign(transform.position.x - Target.transform.position.x), 1, 1);
+    }
+
+    private void Move() 
+    {
+        if (Target == null) return;
+
+        float dist = Vector2.Distance(transform.position, Target.transform.position);
+
+        Vector2 target = (Vector2)Target.transform.position + LateralOffset(); // flang
+        Vector2 arrive = Arrive(target);
+        Vector2 sep = Separation();
+        _velocity += (arrive + sep) * Time.deltaTime;
+        //transform.position += _velocity * Time.deltaTime;
+
+        // атака если близко
+        if (dist < _attackRange)
+        {
+            TryAttack();
+            _velocity = Vector2.zero;
+        }
+
+        // затухание скорости
+        _velocity = Vector2.ClampMagnitude(_velocity, _speed);
+
+        _rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime);
+    }
+
+    private Vector2 LateralOffset()
+    {
+        // Смещение вбок от игрока (чтобы не толпились в одной точке)
+        Vector2 dir = (transform.position - Target.transform.position).normalized;
+        Vector2 right = Vector3.Cross(Vector2.up, dir);
+        float offset = UnityEngine.Random.Range(-1f, 1f) * 1.5f; // боковое смещение
+        return right * offset;
+    }
+
+    private Vector2 Arrive(Vector2 target)
+    {
+        Vector2 toTarget = target - (Vector2)transform.position;
+        float dist = toTarget.magnitude;
+
+        if (dist < 0.1f) 
+            return Vector2.zero;
+
+        float desiredSpeed = (dist < _slowingRadius) ? _speed * (dist / _slowingRadius) : _speed;
+        Vector2 desired = toTarget.normalized * desiredSpeed;
+        Vector2 steer = desired - _velocity;
+
+        return Vector2.ClampMagnitude(steer, _maxForce);
+    }
+
+    private Vector2 Separation()
+    {
+        Vector2 steer = Vector2.zero;
+        int count = 0;
+
+        foreach (var other in Friends)
+        {
+            if (other == null || other == this) 
+                continue;
+
+            float d = Vector2.Distance(transform.position, other.transform.position);
+
+            if (d > 0 && d < _separationRadius)
+            {
+                steer += ((Vector2)transform.position - (Vector2)other.transform.position).normalized / d;
+                count++;
+            }
+        }
+
+        if (count > 0) 
+            steer /= count;
+
+        return steer;
+    }
+
+    void TryAttack()
+    {
+        if (Time.time - _lastAttackTime < _attackCooldown) 
+            return;
+
+        _lastAttackTime = Time.time;
+        Debug.Log($"{gameObject.name} атакует игрока!");
+
+        // Тут можно включить анимацию или вызвать урон игроку
+        // animator.SetTrigger("Attack");
+        // player.GetComponent<PlayerHealth>().TakeDamage(damage);
     }
 }
---
+++ Assets/SilverKZ/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+	[SerializeField] private float _maxY = 1.0f;
+	[SerializeField] private float _minY = -4.5f;
+	[SerializeField] private int _numberOfEnemies = 2;
+	[SerializeField] private float _spawnTime = 1.0f;
+	[SerializeField] private Enemy _prefabEnemy;
+
+	private int _currentEnemies;
+	private int _countAliveEnemies;
+	private Camera _camera;
+	private Player _player;
+	private List<Enemy> _enemies = new List<Enemy>();
+
+	public static Action onStartSpawn;
+	public static Action onAllKill;
+
+	private void Awake()
+    {
+		_camera = Camera.main;
+		_currentEnemies = 0;
+		_countAliveEnemies = _numberOfEnemies;
+	}
+
+	private void OnEnable()
+	{
+		Enemy.onDeathEnemy += UpdateCountEnemy; 
+	}
+
+	private void OnDisable()
+	{
+		Enemy.onDeathEnemy -= UpdateCountEnemy;
+	}
+
+	private void UpdateCountEnemy(int number)
+    {
+		if (_currentEnemies >= _numberOfEnemies)
+		{
+			_countAliveEnemies--;
+
+			if (_countAliveEnemies == 0)
+			{
+				AllKill();
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.TryGetComponent(out Player player))
+		{
+			StartSpawn();
+			_player = player;
+			GetComponent<BoxCollider2D>().enabled = false;
+			_camera.GetComponent<CameraFollow>().maxXAndY.x = transform.position.x + 8.9f;
+			SpawnEnemy();
+		}
+	}
+	
+	private void SpawnEnemy()
+	{
+		bool positionX = UnityEngine.Random.Range(0, 2) == 0 ? true : false;
+		Vector3 spawnPosition;
+		spawnPosition.y = UnityEngine.Random.Range(_minY, _maxY);
+
+		if (positionX)
+		{
+			spawnPosition = new Vector3(transform.position.x + 10, spawnPosition.y, 0);
+		}
+		else
+		{
+			spawnPosition = new Vector3(transform.position.x - 10, spawnPosition.y, 0);
+		}
+
+		Enemy enemy = Instantiate(_prefabEnemy, spawnPosition, Quaternion.identity);
+		enemy.Target = _player;
+		_enemies.Add(enemy);
+		_currentEnemies++;
+		
+		if (_currentEnemies < _numberOfEnemies)
+		{
+			Invoke("SpawnEnemy", _spawnTime);
+		}
+
+		if (_currentEnemies >= _numberOfEnemies)
+		{
+            foreach (var item in _enemies)
+            {
+				item.Friends = _enemies;
+
+			}
+		}
+	}
+
+	public void StartSpawn()
+	{
+		onStartSpawn?.Invoke();
+	}
+
+	public void AllKill()
+	{
+		onAllKill?.Invoke();
+	}
+}
