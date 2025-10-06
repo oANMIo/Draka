3c6003ca85f09318214e15ba46aaef76624f5b16
oANMIo <Ilia.schulmin@gmail.com>
2025-10-06 23:54:11 +0300

    Added Pause menu on Level 1 TEST

---
+++ Assets/Scripts/Canvas scripts/Pause Menu.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu Instance { get; private set; }
+
+    public bool PauseGame { get; private set; }
+    public GameObject PauseGameMenu;
+    public AudioClip clickSound;
+    public float volume = 1f;
+    public GameObject Autors;
+    private bool isAuthorsVisible = false;
+    private AudioSource audioSource;
+    [SerializeField] private GameObject Settings;
+    private bool isSettingsVisible = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.activeSceneChanged += OnSceneChanged;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.playOnAwake = false;
+        audioSource.volume = volume;
+        if (Settings != null)
+        {
+            Settings.SetActive(false);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.activeSceneChanged -= OnSceneChanged;
+    }
+
+    public void Info()
+    {
+        if (Autors != null)
+        {
+            PlayClickSound();
+            Autors.SetActive(true);
+            isAuthorsVisible = true;
+        }
+    }
+
+    public void CloseInfo()
+    {
+        if (isAuthorsVisible)
+        {
+            PlayClickSound();
+            Autors.SetActive(false);
+            isAuthorsVisible = false;
+        }
+    }
+
+    private void OnSceneChanged(Scene previousScene, Scene newScene)
+    {
+        if (newScene.name == "Main Menu")
+        {
+            if (PauseGame) Resume();
+            PauseGameMenu.SetActive(false);
+            enabled = false;
+        }
+        else
+        {
+            enabled = true;
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (!enabled) return;
+
+        Event e = Event.current;
+        if (e.isKey && e.keyCode == KeyCode.Escape && e.type == EventType.KeyUp)
+        {
+            if (PauseGame) Resume();
+            else Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        PlayClickSound();
+        PauseGameMenu.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame = false;
+        Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false;
+    }
+
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None; Cursor.visible = true;
+        PauseGameMenu.SetActive(true);
+        Time.timeScale = 0f;
+        PauseGame = true;
+    }
+
+    public void LoadMenu()
+    {
+        PlayClickSound();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    void PlayClickSound()
+    {
+        if (audioSource != null && clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound, volume);
+        }
+    }
+
+    public void OpenSettings()
+    {
+        PlayClickSound();
+        Settings.SetActive(true);
+        isSettingsVisible = true;
+    }
+
+    public void CloseSettings()
+    {
+        PlayClickSound();
+        Settings.SetActive(false);
+        isSettingsVisible = false;
+    }
+}
